1d5ebf292c1a07f71057f3621959774389e671e4
Oktay <oktay.nabili@prosol.az>
2023-10-01 20:51:48 +0400

    updating VulnerableClass file

--- src/main/java/org/example/VulnerableClass.java
+++ src/main/java/org/example/VulnerableClass.java
@@ -46,7 +46,7 @@ public class VulnerableClass {
         }
         int result = 1;
         for (int i = 2; i <= n; i++) {
-            result *= i;
+            result = multiply(result, i);
         }
         return result;
     }
